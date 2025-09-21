9074b0061275341fee9ac9bfbb652aebf41d2a75
SafalBhandari12 <safalbhandari069@gmail.com>
2025-09-21 19:51:21 +0530

    improved the font

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Products from "@/components/Products";
 import MoreAbout from "@/components/MoreAbout";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Products />
       <MoreAbout />
+      <Footer />
     </>
   );
 }
